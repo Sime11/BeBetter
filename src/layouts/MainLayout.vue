8b29256647745f29e7cc9da03840f1842ecd7b04
Sime <jureskosime@gmail.com>
2024-11-14 21:53:41 +0100

    inicijalizacija

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,40 +2,22 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title class="text-h5 text-bold">BeBetter</q-toolbar-title>
+        <q-space />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer show-if-above>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item v-for="item in linkedList" :key="item.title" clickable v-ripple tag="router-link" :to="item.link">
+          <q-item-section avatar>
+            <q-icon :name="item.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ item.title }}</q-item-label>
+            <q-item-label caption>{{ item.caption }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -45,62 +27,24 @@
   </q-layout>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-defineOptions({
-  name: 'MainLayout'
-})
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+<script>
+export default {
+  name: 'MainLayout',
+  setup () {
+    const linkedList = [
+      { title: 'Naslovnica', caption: 'Početna stranica', icon: 'home', link: '/' },
+      { title: 'Moj profil', caption: 'Vaš profil i ciljevi', icon: 'person', link: '/moj_profil' },
+      { title: 'Dnevnik treninga', caption: 'Unesite svoje treninge', icon: 'fitness_center', link: '/dnevnik_treninga' },
+      { title: 'Planovi treninga', caption: 'Pregled i prilagodba planova', icon: 'assignment', link: '/planovi_treninga' },
+      { title: 'Napredak', caption: 'Analizirajte svoj napredak', icon: 'show_chart', link: '/napredak' },
+      { title: 'Login', caption: 'Prijava', icon: 'login', link: '/login' },
+      { title: 'Registracija', caption: 'Registrirajte se', icon: 'person_add', link: '/registracija' }
+    ];
+
+    return { linkedList };
   }
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+
