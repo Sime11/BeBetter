2413f155c4c92cbfbb5c2c4e64baf59d81547a3c
Sime <jureskosime@gmail.com>
2024-11-14 22:18:18 +0100

    Nove stranice

---
+++ src/pages/NaslovnicaPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="text-h4">Dobrodošli na Naslovnicu BeBetter aplikacije!</div>
+  </q-page>
+</template>
+
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,17 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },                   // Početna stranica
+      { path: '/moj_profil', component: () => import('pages/MojProfil.vue') },        // Moj profil
+      { path: '/dnevnik_treninga', component: () => import('pages/DnevnikTreninga.vue') }, // Dnevnik treninga
+      { path: '/planovi_treninga', component: () => import('pages/PlanoviTreninga.vue') }, // Planovi treninga
+      { path: '/napredak', component: () => import('pages/NapredakPage.vue') },       // Napredak
+      { path: '/login', component: () => import('pages/UserLogin.vue') },             // Login
+      { path: '/registracija', component: () => import('pages/RegistracijaPage.vue') } // Registracija
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  // 404 Not Found page - Always leave this as the last one
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
